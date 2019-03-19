f362392a58452068234edef58e616f83225c3747
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2019-03-19 13:19:35 -0400

    Class 3/19

--- InClass_Homework/PAO/PAO/Form1.vb
+++ InClass_Homework/PAO/PAO/Form1.vb
@@ -9,10 +9,39 @@
 
     Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
 
+        Dim fileOut As IO.StreamWriter
+        fileOut = IO.File.AppendText("PAO.txt")
+        fileOut.WriteLine(lstParties.SelectedItem)
+        fileOut.WriteLine(txtAge.Text)
+        fileOut.Close()
     End Sub
 
     Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
 
+        Dim fileIn As IO.StreamReader
+
+        If IO.File.Exists("PAO.txt") Then
+            fileIn = IO.File.OpenText("PAO.txt")
+            Dim strInput As String, intDem As Integer, intGOP As Integer, intInd As Integer
+            Do Until fileIn.Peek = -1
+                strInput = fileIn.ReadLine()
+                If strInput = "Democrat" Then
+                    intDem += 1
+                ElseIf strInput = "Republican" Then
+                    intGOP += 1
+                Else
+                    intInd += 1
+                End If
+                fileIn.ReadLine()
+            Loop
+            fileIn.Close()
+
+            lblDem.Text = intDem.ToString
+            lblGOP.Text = intGOP.ToString
+            lblInd.Text = intInd.ToString
+        Else
+            MessageBox.Show("Cannot find PAO.txt", "PAO", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
--- student data files/Chap10/CD Solution/CD Project/Main Form.vb
+++ student data files/Chap10/CD Solution/CD Project/Main Form.vb
@@ -2,7 +2,7 @@
 ' Purpose:      Adds and deletes list box entries
 '               Reads information from a sequential access file
 '               Writes information to a sequential access file
-' Programmer:   <your name> on <current date>
+' Programmer:   Branden Barber on March 19, 2019
 
 Option Explicit On
 Option Strict On
@@ -18,14 +18,15 @@ Public Class frmMain
         ' save the list box information
 
         ' declare a StreamWriter variable
-
+        Dim outFile As IO.StreamWriter
         ' open the file for output
-
+        outFile = IO.File.CreateText("CDs.txt")
         ' write each line in the list box
-
-
+        For intIndex As Integer = 0 To lstCds.Items.Count - 1
+            outFile.WriteLine(lstCds.Items(intIndex))
+        Next
         ' close the file
-
+        outFile.Close()
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -33,22 +34,40 @@ Public Class frmMain
         ' stored in a sequential access file
 
         ' declare variables
-
+        Dim inFile As IO.StreamReader
+        'verify that the file exists
+        If IO.File.Exists("CDs.txt") Then
+            'open the file for input
+            inFile = IO.File.OpenText("CDs.txt")
+            Do Until inFile.Peek = -1
+                lstCds.Items.Add(inFile.ReadLine)
+            Loop
+            inFile.Close()
+        Else
+            MessageBox.Show("Can't find the CDs.txt file", "CD Collection", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         ' adds CD information to the list box
 
         ' declare variables
-
+        Dim strName As String, strArtist As String, strPrice As String, strDate As String
+        Dim strConcatenatedInfo As String, dblPrice As Double
         ' get the CD information
-
+        strName = InputBox("CD name:", "CD Collection")
+        strArtist = InputBox("Artist", "CD Collection")
+        strPrice = InputBox("Price", "CD Collection")
+        strDate = InputBox("Date Purchased", "CD Collection")
         ' format the price, then concatenate the
         ' input items, using 40 spaces for the 
         ' CD name, 25 spaces for the artist name,
         ' and 5 spaces for the price
-
+        Double.TryParse(strPrice, dblPrice)
+        strPrice = dblPrice.ToString("N2")
+        strConcatenatedInfo = strName.PadRight(27) & strDate.PadRight(13) & strArtist.PadRight(25) & strPrice.PadLeft(5)
         ' add the information to the list box
+        lstCds.Items.Add(strConcatenatedInfo)
 
     End Sub
 
@@ -56,6 +75,8 @@ Public Class frmMain
         ' removes the selected line from the list box
 
         ' if a line is selected, remove the line
-
+        If lstCds.SelectedIndex <> -1 Then
+            lstCds.Items.Remove(lstCds.SelectedIndex)
+        End If
     End Sub
 End Class
